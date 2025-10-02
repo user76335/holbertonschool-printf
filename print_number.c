38e58744a47c7d4f721b2571969a731eeb64c5f1
11968-ship-it <11968@holbertonstudents.com>
2025-10-02 10:37:55 +0300

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_number - Recursively prints an unsigned int
- * @n: Unsigned int to print
+ * print_number - Prints an unsigned integer recursively
+ * @n: Number to print
  * Return: Number of characters printed
  */
 int print_number(unsigned int n)
 {
-	int count = 0;
+    int count = 0;
 
-	if (n / 10)
-		count += print_number(n / 10);
+    if (n / 10)
+        count += print_number(n / 10);
 
-	count += _putchar((n % 10) + '0');
-	return (count);
+    count += _putchar((n % 10) + '0');
+    return (count);
 }
