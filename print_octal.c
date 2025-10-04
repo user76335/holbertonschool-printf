f2b2580523c5060776c2a6443be588844dc7fc14
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 23:20:36 +0300

    Update print_octal.c

--- print_octal.c
+++ print_octal.c
@@ -1,5 +1,30 @@
 #include "main.h"
 
+/**
+ * print_octal_number - Recursively prints an unsigned int in octal
+ * @n: The number to print
+ *
+ * Return: Number of characters printed
+ */
+int print_octal_number(unsigned int n)
+{
+    int count = 0;
+
+    if (n / 8)
+        count += print_octal_number(n / 8);
+
+    count += _putchar((n % 8) + '0');
+
+    return count;
+}
+
+/**
+ * print_octal - Prints an unsigned int in octal with '#' flag support
+ * @n: The number to print
+ * @flags: Flags struct containing the hash flag
+ *
+ * Return: Number of characters printed
+ */
 int print_octal(unsigned int n, flags_t flags)
 {
     int count = 0;
@@ -7,7 +32,7 @@ int print_octal(unsigned int n, flags_t flags)
     if (flags.hash && n != 0)
         count += _putchar('0');
 
-    count += print_base(n, 8, 0);
+    count += print_octal_number(n);
+
     return count;
 }
-
