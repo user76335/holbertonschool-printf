0d06c581be9bc9b3451d46aac130c88d9cf6798c
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 23:20:03 +0300

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,3 +1,30 @@
+#include "main.h"
+
+/**
+ * print_unsigned - Prints an unsigned integer recursively
+ * @n: The unsigned integer to print
+ *
+ * Return: Number of characters printed
+ */
+int print_unsigned(unsigned int n)
+{
+    int count = 0;
+
+    if (n / 10)
+        count += print_unsigned(n / 10);
+
+    count += _putchar((n % 10) + '0');
+
+    return count;
+}
+
+/**
+ * print_number - Prints a signed integer with flags + and space
+ * @n: The signed integer to print
+ * @flags: Flags struct with plus and space flags set
+ *
+ * Return: Number of characters printed
+ */
 int print_number(int n, flags_t flags)
 {
     int count = 0;
@@ -18,5 +45,6 @@ int print_number(int n, flags_t flags)
     }
 
     count += print_unsigned(num);
+
     return count;
 }
