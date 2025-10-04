f0359bd93e798908750a01ad07c6b53e06f27c36
user76335 <11958@holbertonstudents.com>
2025-10-04 18:21:02 +0000

    update printf

--- _printf.c
+++ _printf.c
@@ -1,68 +1,51 @@
 #include "main.h"
 
-/**
- * handle_format - Handle a format specifier and print accordingly
- * @format_char: The format specifier character
- * @args: va_list of arguments
- * Return: Number of characters printed
- */
-int handle_format(char format_char, va_list args)
-{
-	if (format_char == 'c')
-		return (print_char(args));
-	if (format_char == 's')
-		return (print_string(args));
-	/*--*/
-	/*-placed-*/
-	if (format_char == 'd' || format_char == 'i')
-        return (print_int(args));
-	
-	/*-placed-*/
-	if (format_char == 'b')
-		return (print_binary(args));
-	if (format_char == '%')
-		return (write(1, "%", 1));
-
-	/* Unknown specifier: print '%' and the character */
-	write(1, "%", 1);
-	return (write(1, &format_char, 1) + 1);
-}
-
-/**
- * _printf - Produces output according to a format
- * @format: Format string
- * Return: Number of characters printed
- */
+/* Main _printf function */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, printed = 0, res;
+    va_list args;
+    int i = 0, len = 0;
 
-	if (format == NULL)
-		return (-1);
+    if (!format)
+        return -1;
 
-	va_start(args, format);
-
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			res = handle_format(format[i], args);
-			printed += res;
-		}
-		else
-		{
-			printed += write(1, &format[i], 1);
-		}
-		i++;
-	}
-
-	va_end(args);
-	return (printed);
+    va_start(args, format);
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+                len += _putchar(va_arg(args, int));
+            else if (format[i] == 's')
+                len += _puts(va_arg(args, char *));
+            else if (format[i] == '%')
+                len += _putchar('%');
+            else if (format[i] == 'd' || format[i] == 'i')
+                len += print_number(va_arg(args, int));
+            else if (format[i] == 'b')
+                len += print_binary(va_arg(args, unsigned int));
+            else if (format[i] == 'u')
+                len += print_unsigned(va_arg(args, unsigned int), 10, 0);
+            else if (format[i] == 'o')
+                len += print_unsigned(va_arg(args, unsigned int), 8, 0);
+            else if (format[i] == 'x')
+                len += print_unsigned(va_arg(args, unsigned int), 16, 0);
+            else if (format[i] == 'X')
+                len += print_unsigned(va_arg(args, unsigned int), 16, 1);
+            else
+            {
+                len += _putchar('%');
+                if (format[i])
+                    len += _putchar(format[i]);
+                else
+                    i--;
+            }
+        }
+        else
+            len += _putchar(format[i]);
+        i++;
+    }
+    va_end(args);
+    return len;
 }
