1aa2b5c1c08558c21422f0355b65733e9c160d94
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 23:14:27 +0300

    Update main.h

--- main.h
+++ main.h
@@ -3,18 +3,28 @@
 
 #include <stdarg.h>
 
+/* Flag structure */
+typedef struct flags_s
+{
+    int plus;
+    int space;
+    int hash;
+} flags_t;
+
+
 /* _printf function */
 int _printf(const char *format, ...);
 
-/* helper functions */
+/* Helper functions */
 int _putchar(char c);
 int print_char(char c);
 int print_string(char *s);
-int print_number(int n);
+int print_number(int n, flags_t flags);
 int print_unsigned(unsigned int n);
-int print_octal(unsigned int n);
+int print_octal(unsigned int n, flags_t flags);
 int print_binary(unsigned int n);
-int print_hex(unsigned int n, int uppercase);
+int print_hex(unsigned int n, int uppercase, flags_t flags);
 int print_address(void *p);
 
 #endif /* MAIN_H */
+
