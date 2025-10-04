6e552828254c7ff753cc3646086d513e4cb69c0e
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 20:12:56 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -64,3 +64,36 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (printed);
 }
+
+
+/**
+ * print_binary - Prints the binary representation of an unsigned int
+ * @args: va_list containing the unsigned int to print
+ *
+ * Return: Number of characters printed
+ */
+int print_binary(va_list args)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	int count = 0;
+	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
+	int started = 0;
+
+	if (num == 0)
+		return (write(1, "0", 1));
+
+	while (mask > 0)
+	{
+		if (num & mask)
+		{
+			started = 1;
+			count += write(1, "1", 1);
+		}
+		else if (started)
+		{
+			count += write(1, "0", 1);
+		}
+		mask >>= 1;
+	}
+	return (count);
+}
