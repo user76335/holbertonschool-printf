321f924fa5fb709b99760e59013ec9de8bdb71d6
user76335 <11958@holbertonstudents.com>
2025-10-04 18:02:40 +0000

    update printf

--- _printf.c
+++ _printf.c
@@ -1,75 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-
-/* Print a single character */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-/* Print a string */
-int _puts(char *str)
-{
-    int len = 0;
-
-    if (!str)
-        str = "(null)";
-    while (*str)
-    {
-        len += _putchar(*str);
-        str++;
-    }
-    return len;
-}
-
-/* Print signed integer */
-int print_number(int n)
-{
-    int len = 0;
-    unsigned int num;
-
-    if (n < 0)
-    {
-        len += _putchar('-');
-        num = -n;
-    }
-    else
-        num = n;
-
-    if (num / 10)
-        len += print_number(num / 10);
-    len += _putchar(num % 10 + '0');
-
-    return len;
-}
-
-/* Print unsigned int in given base */
-int print_unsigned(unsigned int n, int base, int uppercase)
-{
-    char buffer[32];
-    char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
-    int i = 0, len = 0;
-
-    if (n == 0)
-        return _putchar('0');
-
-    while (n)
-    {
-        buffer[i++] = digits[n % base];
-        n /= base;
-    }
-
-    while (i--)
-        len += _putchar(buffer[i]);
-
-    return len;
-}
-
-/* Print unsigned int in binary */
-int print_binary(unsigned int n)
-{
-    return print_unsigned(n, 2, 0);
-}
 
 /* The main _printf function */
 int _printf(const char *format, ...)
@@ -120,4 +49,3 @@ int _printf(const char *format, ...)
     va_end(args);
     return len;
 }
-
