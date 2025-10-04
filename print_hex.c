d147cbd675a73636b6fbfa3272970c33f5b17e7f
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 23:12:00 +0300

    Update print_hex.c

--- print_hex.c
+++ print_hex.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
-/**
- * print_hex - prints an unsigned int in hexadecimal
- * @n: number
- * @uppercase: 1 for uppercase, 0 for lowercase
- *
- * Return: number of characters printed
- */
-int print_hex(unsigned int n, int uppercase)
+int print_hex(unsigned int n, int uppercase, flags_t flags)
 {
     int count = 0;
-    char c;
-    char *hex = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
 
-    if (n / 16)
-        count += print_hex(n / 16, uppercase);
+    if (flags.hash && n != 0)
+    {
+        count += _putchar('0');
+        count += _putchar(uppercase ? 'X' : 'x');
+    }
 
-    c = hex[n % 16];
-    count += _putchar(c);
-
-    return (count);
+    count += print_base(n, 16, uppercase); /* You must have this utility function */
+    return count;
 }
+
 
