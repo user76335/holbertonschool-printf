4c0cbf1cb82732859ac16a703fd7240202cfe135
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 23:09:02 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,15 +2,53 @@
 #include <stdarg.h>
 
 /**
- * _printf - custom printf implementation
- * @format: format string
+ * struct flags_s - structure to hold flag info
+ * @plus: Plus flag (+)
+ * @space: Space flag (' ')
+ * @hash: Hash flag (#)
+ */
+typedef struct flags_s
+{
+    int plus;
+    int space;
+    int hash;
+} flags_t;
+
+/**
+ * parse_flags - Parses flag characters and sets the flags struct
+ * @format: Pointer to the format string
+ * @flags: Pointer to flags struct to fill
+ * Return: Number of flag characters parsed
+ */
+int parse_flags(const char *format, flags_t *flags)
+{
+    int i = 0;
+
+    while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+    {
+        if (format[i] == '+')
+            flags->plus = 1;
+        else if (format[i] == ' ')
+            flags->space = 1;
+        else if (format[i] == '#')
+            flags->hash = 1;
+        i++;
+    }
+
+    return i;
+}
+
+/**
+ * _printf - Custom printf implementation with +, space, and # flags
+ * @format: Format string
  *
- * Return: number of characters printed
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
     va_list args;
     int count = 0;
+    flags_t flags;
 
     if (!format)
         return (-1);
@@ -22,6 +60,16 @@ int _printf(const char *format, ...)
         if (*format == '%')
         {
             format++;
+            if (!*format)
+                break;
+
+            /* Reset and parse flags */
+            flags.plus = 0;
+            flags.space = 0;
+            flags.hash = 0;
+            format += parse_flags(format, &flags);
+
+            /* Handle format specifier */
             switch (*format)
             {
                 case 'c':
@@ -35,19 +83,19 @@ int _printf(const char *format, ...)
                     break;
                 case 'd':
                 case 'i':
-                    count += print_number(va_arg(args, int));
+                    count += print_number(va_arg(args, int), flags);
                     break;
                 case 'u':
                     count += print_unsigned(va_arg(args, unsigned int));
                     break;
                 case 'o':
-                    count += print_octal(va_arg(args, unsigned int));
+                    count += print_octal(va_arg(args, unsigned int), flags);
                     break;
                 case 'x':
-                    count += print_hex(va_arg(args, unsigned int), 0);
+                    count += print_hex(va_arg(args, unsigned int), 0, flags);
                     break;
                 case 'X':
-                    count += print_hex(va_arg(args, unsigned int), 1);
+                    count += print_hex(va_arg(args, unsigned int), 1, flags);
                     break;
                 case 'b':
                     count += print_binary(va_arg(args, unsigned int));
