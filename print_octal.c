76fbe5fc973cd48b10142163e1e2a6c446271942
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 23:11:22 +0300

    Update print_octal.c

--- print_octal.c
+++ print_octal.c
@@ -1,21 +1,13 @@
 #include "main.h"
 
-/**
- * print_octal - prints an unsigned integer in octal
- * @n: number to print
- *
- * Return: number of characters printed
- */
-int print_octal(unsigned int n)
+int print_octal(unsigned int n, flags_t flags)
 {
     int count = 0;
-    char c;
 
-    if (n / 8)
-        count += print_octal(n / 8);
+    if (flags.hash && n != 0)
+        count += _putchar('0');
 
-    c = (n % 8) + '0';
-    count += _putchar(c);
-
-    return (count);
+    count += print_base(n, 8, 0);
+    return count;
 }
+
