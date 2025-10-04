bb0db6d0281ab450639636f8679129f0c225ef6a
11968-ship-it <11968@holbertonstudents.com>
2025-10-04 23:09:51 +0300

    Update print_number.c

--- print_number.c
+++ print_number.c
@@ -1,32 +1,22 @@
-#include "main.h"
-
-/**
- * print_number - prints an integer
- * @n: number
- *
- * Return: number of characters printed
- */
-int print_number(int n)
+int print_number(int n, flags_t flags)
 {
     int count = 0;
-    char c;
+    unsigned int num;
 
-    if (n < 0)
+    if (n >= 0)
+    {
+        if (flags.plus)
+            count += _putchar('+');
+        else if (flags.space)
+            count += _putchar(' ');
+        num = n;
+    }
+    else
     {
         count += _putchar('-');
-        if (n == -2147483648)
-        {
-            count += print_string("2147483648");
-            return (count);
-        }
-        n = -n;
+        num = -n;
     }
 
-    if (n / 10)
-        count += print_number(n / 10);
-
-    c = (n % 10) + '0';
-    count += _putchar(c);
-
-    return (count);
+    count += print_unsigned(num);
+    return count;
 }
